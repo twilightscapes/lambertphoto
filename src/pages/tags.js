a9e6175b32492493e4b7f937147be6e23429256f
Twilight <ihatespam@gmail.com>
2023-11-30 21:44:21 -0600

    tag fix

--- src/pages/tags.js
+++ src/pages/tags.js
@@ -61,7 +61,7 @@ const TagIndex = ({ data }) => {
 
        
 
-        <div className="contentpanel grid-container" style={{ marginTop: "" }}>
+        <div className="contentpanel grid-container" style={{justifyContent:'center', alignItems:'center', marginTop:''}}>
           <div className="sliderSpacer" style={{ height: "", paddingTop: "", display: "" }}></div>
    
           {data.allMarkdownRemark.edges &&
@@ -74,52 +74,57 @@ const TagIndex = ({ data }) => {
                 // const { featuredImage } = node.frontmatter;
 
                 return (
-                  <div key={node.fields.slug} className="post-card1" style={{ justifyContent: "center", alignItems: "center" }}>
+                  <div key={node.fields.slug} className="post-card1" style={{  alignItems: "center" }}>
       
                     <Link className="postlink" to={node.fields.slug}>
 
 {node.frontmatter.featuredImage ? (
     <GatsbyImage
-      image={node.frontmatter.featuredImage.childImageSharp.gatsbyImageData}
-      alt={node.frontmatter.title + " - Featured image"}
-      className="featured-image1"
-      placeholder="blurred"
-      loading="eager"
-      style={{ position: 'relative', zIndex: '1', maxHeight: '', margin: '0 auto' }}
-    />
+    image={node.frontmatter.featuredImage.childImageSharp.gatsbyImageData}
+    alt={node.frontmatter.title + " - Featured image"}
+    className="featured-image1"
+    placeholder="blurred"
+    loading="eager"
+    style={{ position: 'relative', zIndex: '1', maxHeight: '', margin: '0 auto' }}
+  />
+
 ) : (
 
-    <StaticImage
-      className="featured-image1"
-      src="../../static/assets/default-og-image.webp"
-      alt="Default Image"
-      style={{ position: 'relative', zIndex: '' }}
-    />
+  <StaticImage
+    className="featured-image1"
+    src="../../../static/assets/default-og-image.webp"
+    alt="Default Image"
+    style={{ position: 'relative', zIndex: '' }}
+  />
 
 )}
 
 
-<div className="post-content" style={{display:'flex', flexDirection:'column', justifyContent:'start', width:'100%', height:'', position:'relative', background:'', padding:'0', margin:'0 auto 0 auto', textAlign:'center', overFlow:'hidden'}}>
+<div className="post-content" style={{display:'flex', flexDirection:'column', justifyContent:'center', width:'100%', height:'', position:'relative', background:'', padding:'0', margin:'0 auto 0 auto', textAlign:'center', overFlow:'hidden'}}>
 
   {node.frontmatter.youtube.youtuber ? (
 
-<div className="spotlight" style={{border:'0px solid green', }}>
-<div className="posticons" style={{flexDirection:'column', justifyContent:'center', margin:'0 auto'}}>
+<div className="spotlight" style={{marginLeft:'10%', marginTop:'-28%', margin:'-24% 10% 0 10%'}}>
+
+<div className="posticons" style={{flexDirection:'column', margin:'0 auto'}}>
+
 <div style={{display:'flex', justifyContent:'space-around', gap:'2vw', color:'fff', }}>
 <FaImage className="posticon" style={{margin:'0 auto', width:'60%', height:'30px', fontSize:''}} />
 <ImPlay className="posticon" style={{margin:'0 auto', width:'60%', height:'30px', fontSize:''}} />
 <AiOutlinePicLeft className="posticon" style={{margin:'0 auto', width:'60%', height:'30px', fontSize:''}} />
 </div>
+
 Play Multimedia
 </div>
+
 </div>
 
 ) : (
 ""
 )}
 
-<div className="panel" style={{display:'flex', justifyContent:'space-between', alignItems:'center', margin:'10px auto', width:'auto', maxWidth:'80vw', gap:'.4vw', height:'', textAlign:'center', padding:'1vh 2vw', fontSize:'clamp(1rem, 1vw, 1rem)',  background:'rgba(0, 0, 0, 0.7)', borderRadius:'', border:'0px solid red', color:'#aaa' }}>
-      <h2 className="title" style={{ }}>
+<div className="panel" style={{display:'flex', justifyContent:'space-between', alignItems:'center', margin:'10px auto', maxWidth:'80vw', gap:'.4vw', height:'', textAlign:'center', padding:'1vh 2vw', fontSize:'clamp(1rem, 1vw, 1rem)',  background:'rgba(0, 0, 0, 0.7)', borderRadius:'', color:'#aaa' }}>
+<h2 className="title1" style={{ }}>
         {node.frontmatter.title}
       </h2>
   </div>
@@ -188,7 +193,6 @@ export const query = graphql`
             featuredImage {
               childImageSharp {
                 gatsbyImageData(
-                  width: 800
                   quality: 80
                   placeholder: BLURRED
                   formats: [AUTO, WEBP, AVIF]
