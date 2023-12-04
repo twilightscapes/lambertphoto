6d2c5eb04c745010bc1339e87eb84d425a887507
Twilight <ihatespam@gmail.com>
2023-12-03 23:47:27 -0600

    Update update-theme.sh

--- update-theme.sh
+++ update-theme.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
-# Set the URL of your central repository git
+# Set the URL of your central repository
 THEME_REPO_URL="https://github.com/piratesocial/pirate.git"
 
 # Set the branch or tag you want to pull updates from
 BRANCH_OR_TAG="main"
 
-# Temporarily store the user's changes (optional)
-mv src user_src_backup
+# Debug statement
+echo "Starting update process..."
 
 # Clone the central repository
 git clone --branch $BRANCH_OR_TAG --depth 1 $THEME_REPO_URL tmp_theme
+
+# Debug statement
+echo "Cloned central repository..."
 
 # Replace the src folder
 rm -rf src
 mv tmp_theme/src .
 
-# Copy the package.json file
-cp tmp_theme/package.json .
+# Debug statement
+echo "Replaced src folder..."
 
 # Clean up
 rm -rf tmp_theme
-rm -rf user_src_backup
 
 echo "Theme updated successfully!"
